594cca807e0a329bcf9f448a284ffaefa4779b47
alvarodltp <alvarodltp@gmail.com>
2018-12-26 21:10:12 -0500

    first heroku commit

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,5 +1,5 @@
 class StatsController < ApplicationController
-  # before_action :find_stat
+  before_action :find_stat, only: [:update]
 
   def index
     render json: Stat.all
@@ -13,9 +13,22 @@ class StatsController < ApplicationController
     render json: Stat.create(stat_params)
   end
 
+  def update
+    @stat.update(stat_params)
+    if @stat.save
+      render json: @stat, status: :accepted
+    else
+      render json: { errors: @stat.errors.full_messages }, status: :unprocessible_entity
+    end
+  end
+
   private
   def stat_params
     params.require(:stat).permit(:user_id, :date, :calories_to_maintain, :calories_for_goal, :bmr, :protein_grams, :carb_grams, :fat_grams, :protein_percentage, :carb_percentage, :fat_percentage)
+  end
+
+  def find_stat
+    @stat = Stat.find(params[:id])
   end
 
 end
