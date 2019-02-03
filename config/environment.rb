5b459973f5f5b05a847d22591c09bfabc56fd3c2
alvarodltp <alvarodltp@gmail.com>
2019-02-02 23:17:42 -0500

    credentials set up for email test

--- config/environment.rb
+++ config/environment.rb
@@ -5,8 +5,8 @@ require_relative 'application'
 Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
-  :user_name => ENV["MAIL_USERNAME"],
-  :password => ENV["MAIL_PASSWORD"],
+  :user_name => "alvarodltp",
+  :password => "Alvarofitness22!",
   :domain => 'https://www.fitcalculations.com/bmr-calories-macros',
   :address => 'smtp.sendgrid.net',
   :port => 587,
