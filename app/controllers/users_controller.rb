dc9d951394cac65f1b37d1c68a46af66ebeb6896
alvarodltp <alvarodltp@gmail.com>
2019-02-02 23:11:12 -0500

    debugging user controller email

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,10 +17,10 @@ class UsersController < ApplicationController
     @user.update(user_params)
     if @user.save
       render json: @user, status: :accepted
+      redirect_to(@user, :notice => 'User created')
       if @user["email"] != ""
       # Deliver the signup email
       UserNotifierMailer.send_signup_email(@user).deliver
-      # redirect_to(@user, :notice => 'User created')
     else
       render json: { errors: @user.errors.full_messages }, status: :unprocessible_entity
       end
