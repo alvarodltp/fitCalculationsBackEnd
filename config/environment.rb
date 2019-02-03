126f1fb206fab7d6d5617f6f5b67e34f4409b4db
alvarodltp <alvarodltp@gmail.com>
2019-02-02 23:23:07 -0500

    removes credentials

--- config/environment.rb
+++ config/environment.rb
@@ -5,8 +5,8 @@ require_relative 'application'
 Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
-  :user_name => "alvarodltp",
-  :password => "Alvarofitness22!",
+  :user_name => "",
+  :password => "",
   :domain => 'https://www.fitcalculations.com/bmr-calories-macros',
   :address => 'smtp.sendgrid.net',
   :port => 587,
