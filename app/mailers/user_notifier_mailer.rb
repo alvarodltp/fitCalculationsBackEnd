2f62b3bb842863a1501d286e784b1eacb003ac51
alvarodltp <alvarodltp@gmail.com>
2019-02-08 23:57:34 -0500

    remove method to save user to list

--- app/mailers/user_notifier_mailer.rb
+++ app/mailers/user_notifier_mailer.rb
@@ -1,5 +1,5 @@
 class UserNotifierMailer < ApplicationMailer
-  require 'rest_client'
+  # require 'rest_client'
   default :from => 'fitcalculations@gmail.com'
   # send a signup email to the user, pass in the user object that   contains the user's email address
   def send_signup_email(user)
