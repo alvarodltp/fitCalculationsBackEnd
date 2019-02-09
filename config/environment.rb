d31fb907dca7ee8728f0d505b4bb8b8ff51fd947
alvarodltp <alvarodltp@gmail.com>
2019-02-08 21:58:33 -0500

    Sets environment variables for email

--- config/environment.rb
+++ config/environment.rb
@@ -5,8 +5,8 @@ require_relative 'application'
 Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
-  :user_name => "",
-  :password => "",
+  :user_name => ENV["MAIL_USERNAME"],
+  :password => ENV["MAIL_PASSWORD"],
   :domain => 'https://www.fitcalculations.com/bmr-calories-macros',
   :address => 'smtp.sendgrid.net',
   :port => 587,
