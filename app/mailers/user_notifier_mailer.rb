0a466034094d39e282380b27fbefc56aae2c8e9c
alvarodltp <alvarodltp@gmail.com>
2019-02-16 19:35:21 -0500

    initialize active campaign variable in environment

--- app/mailers/user_notifier_mailer.rb
+++ app/mailers/user_notifier_mailer.rb
@@ -1,8 +1,5 @@
 class UserNotifierMailer < ApplicationMailer
   # require 'rest_client'
-  active_campaign = ActiveCampaign.new(
-    api_endpoint: ENV['END_POINT'], # e.g. 'https://yourendpoint.api-us1.com'
-    api_key: ENV['ACTIVE_CAMPAING_API']) # e.g. 'a4e60a1ba200595d5cc37ede5732545184165e'
   # send a signup email to the user, pass in the user object that   contains the user's email address
   # def add_user_to_list(user)
   #   @user = user
