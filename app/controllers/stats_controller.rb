eb02fd18ef9bb8a3c1c9a34fa4e65fc174592074
Alvaro <alvarodltp@gmail.com>
2019-03-27 00:29:35 -0400

    adds method to add contact

--- app/controllers/stats_controller.rb
+++ app/controllers/stats_controller.rb
@@ -1,6 +1,5 @@
 class StatsController < ApplicationController
   before_action :find_stat, only: [:update]
-  before_action :find_user, only: [:create]
 
   def index
     render json: Stat.all
@@ -12,30 +11,7 @@ class StatsController < ApplicationController
 
   def create
     render json: Stat.create(stat_params)
-    client.contact_add(
-      "p[1]" => 1,
-      "first_name" => @user.name,
-      "email" => @user.email,
-      "field[1,0]" => @stat[0].age,
-      "field[2,0]" => @gender,
-      "field[3,0]" => @stat[0].activity_level,
-      "field[4,0]" => @stat[0].body_type,
-      "field[5,0]" => @stat[0].weight_in_lb,
-      "field[6,0]" => @stat[0].height_in_feet,
-      "field[7,0]" => @stat[0].diet_type,
-      "field[8,0]" => @stat[0].reason_to_get_fit,
-      "field[9,0]" => @stat[0].calories_for_goal,
-      "field[10,0]" => @stat[0].bmr,
-      "field[11,0]" => @stat[0].bmi,
-      "field[12,0]" => @stat[0].calories_to_maintain,
-      "field[13,0]" => @stat[0].protein_grams,
-      "field[14,0]" => @stat[0].carb_grams,
-      "field[15,0]" => @stat[0].fat_grams,
-      "field[16,0]" => @stat[0].protein_percentage,
-      "field[17,0]" => @stat[0].carb_percentage,
-      "field[18,0]" => @stat[0].fat_percentage,
-      "field[19,0]" => @stat[0].height_in_inches,
-      "field[20,0]" => @stat[0].goal)
+    User.add_contact
   end
 
   def update
@@ -54,9 +30,5 @@ class StatsController < ApplicationController
 
   def find_stat
     @stat = Stat.find(params[:id])
-  end
-
-  def find_user
-    @user = Stat.user
   end
 end
