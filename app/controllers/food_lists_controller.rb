cb809e4753acef9f79373e34ac721fa369cb26f5
Alvaro De La Torre <alvarodltp@gmail.com>
2019-04-17 14:35:16 -0400

    adds create and update actions to food list controller

--- app/controllers/food_lists_controller.rb
+++ app/controllers/food_lists_controller.rb
@@ -1,4 +1,5 @@
 class FoodListsController < ApplicationController
+  before_action :find_food_list, only: [:update]
 
   def index
     render json: FoodList.all
@@ -8,4 +9,25 @@ class FoodListsController < ApplicationController
     render json: FoodList.find(params[:id])
   end
 
+  def create
+    render json: FoodList.create(food_list_params)
+  end
+
+  def update
+    @food_list.update(food_list_params)
+    if @food_list.save
+      render json: @food_list, status: :accepted
+    else
+      render json: { errors: @food_list.errors.full_messages }, status: :unprocessible_entity
+    end
+  end
+
+  private
+  def food_list_params
+    params.require(:food_list).permit(:date, :user_id)
+  end
+
+  def find_food_list
+    @food_list = FoodList.find(params[:id])
+  end
 end
