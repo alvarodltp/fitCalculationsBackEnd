cabbb6d570f74f231e4051b0ac6259f47b67a8f5
Alvaro <alvarodltp@gmail.com>
2019-05-14 16:56:04 -0400

    adds create action to food controller

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -7,4 +7,14 @@ class FoodsController < ApplicationController
   def show
     render json: Food.find(params[:id])
   end
+
+  def create
+    render json: Food.create(food_params)
+  end
+
+  private
+  def food_params
+    params.require(:food).permit(:name, :category, :food_list_id)
+  end
+
 end
