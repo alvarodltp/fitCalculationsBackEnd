6d20f089f5d109fb527c29bead4031acc6e789dd
Alvaro <alvarodltp@gmail.com>
2019-05-14 11:52:38 -0400

    adds food_list_id to foods

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_30_012500) do
+ActiveRecord::Schema.define(version: 2019_05_14_143956) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(version: 2019_04_30_012500) do
     t.string "category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "food_list_id"
   end
 
   create_table "stats", force: :cascade do |t|
