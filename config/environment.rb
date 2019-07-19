3099101aa221393777f56ad7eda81ddeb07327ec
Alvaro De La Torre <alvarodltp@gmail.com>
2019-07-19 12:01:37 -0400

    adds jwt gem

--- config/environment.rb
+++ config/environment.rb
@@ -7,16 +7,5 @@ Rails.application.initialize!
 client = ActiveCampaign.new(
   api_endpoint: ENV['END_POINT'],
   api_key: ENV['ACTIVE_CAMPAING_API'])
-# client = ::ActiveCampaign::Client.new(
-#         api_endpoint: ENV['END_POINT'], # e.g. 'https://yourendpoint.api-us1.com/admin/api.php'
-#         api_key: ENV['ACTIVE-CAMPAIGN-API']) # e.g. 'a4e60a1ba200595d5cc37ede5732545184165e'
 
-# ActionMailer::Base.smtp_settings = {
-#   :user_name => ENV["MAIL_USERNAME"],
-#   :password => ENV["MAIL_PASSWORD"],
-#   :domain => 'https://www.fitcalculations.com/bmr-calories-macros',
-#   :address => 'smtp.sendgrid.net',
-#   :port => 587,
-#   :authentication => :plain,
-#   :enable_starttls_auto => true
-# }
+Dir[Rails.root.join('lib/**/*.rb')].each { |f| require f }
