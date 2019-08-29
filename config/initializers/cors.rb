0eb7d3cc7fc44cec489d93c31cb2738a9f8d9208
Alvaro De La Torre <alvarodltp@gmail.com>
2019-08-29 13:02:55 -0400

    adds origin to rails api

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'https://www.fitcalculations.com/'
 
     resource '*',
       headers: :any,
